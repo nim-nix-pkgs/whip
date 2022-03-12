2ad0a0a3303a2d459dad52b93a3af5e23453eaa7
hugosenari <hugosenari@gmail.com>
2022-03-12 02:06:20 -0300

    chore: re index whip

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = ''Whip is high performance web application server based on httpbeast a nest for redix tree based routing with some extra opmtizations.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."whip-master".url = "path:./master";
+  inputs."whip-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."whip-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."whip-0_2".url = "path:./0_2";
+  inputs."whip-0_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."whip-0_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
